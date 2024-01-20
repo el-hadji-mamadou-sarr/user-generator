43974fbd6fdb2c7c64cc44eedac744d8d1b79870
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-01-20 19:34:01 +0100

    adding skills

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
 import fs from "fs/promises";
-
+import { skills } from "./skills.js";
 async function generateData() {
   for (let id = 0; id < 100; id++) {
     const data = createData(id);
@@ -12,18 +12,13 @@ async function add(data) {
   const filename = "data.json";
 
   try {
-    // Step 1: Read the existing data from the JSON file
     const fileData = await fs.readFile(filename, "utf8");
-    // Step 2: Parse the JSON data into a JavaScript object
     const jsonData = JSON.parse(fileData);
 
-    // Step 3: Append or modify the data as needed
     jsonData.push(data);
 
-    // Step 4: Convert the JavaScript object back to a JSON string
     const updatedJsonString = JSON.stringify(jsonData, null, 2);
 
-    // Step 5: Write the updated JSON string back to the file
     await fs.writeFile(filename, updatedJsonString, "utf8");
 
     console.log("Data appended successfully!");
@@ -32,6 +27,13 @@ async function add(data) {
   }
 }
 
+function generateSkills() {
+  const user_skills = [];
+  for (let i = 0; i <= Math.floor(Math.random() * 6); i++) {
+    user_skills.push(skills[Math.floor(Math.random() * skills.length)]);
+  }
+  return user_skills;
+}
 function createData(id) {
   const first_name = faker.person.firstName();
   const last_name = faker.person.lastName();
@@ -55,6 +57,7 @@ function createData(id) {
   for (let i = 0; i <= Math.floor(Math.random() * 6); i++) {
     photos.push(faker.image.urlPicsumPhotos({ width: 373, height: 556 }));
   }
+  const user_skills = generateSkills();
 
   const data = {
     id,
@@ -72,6 +75,7 @@ function createData(id) {
     created_at,
     updated_at,
     photos,
+    user_skills,
     socials: {
       personal_website,
       twitter,
